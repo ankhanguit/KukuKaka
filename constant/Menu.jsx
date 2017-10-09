19f0209255c5c32b980340940d67f113e4d2b886
ankhanguit <anhkhanguit@gmail.com>
2017-10-09 19:57:18 +0700

    Create Menu.jsx

---
+++ constant/Menu.jsx
@@ -0,0 +1,17 @@
+export const Menu = {
+    master: [
+        {id: "abcdeg", name: "sdf"}
+    ]
+  , prepatation: [
+        {id: "abc", name: "sdf}
+      , {id: "abcdes", name: "sdf"}
+    ]
+  , print: [
+        {id: "abc", name: "ádfád"}
+      , {id: "abc", name: "ádfá"}
+      , {id: "abcded", name: "ádfsadfdf"}
+    ]
+  , setting: [
+      {id: "abcdef", name: "sdfádf"}
+  ]
+};
