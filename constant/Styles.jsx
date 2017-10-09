3681786679f46ccbe8d7f430018525ea9dccb85a
ankhanguit <anhkhanguit@gmail.com>
2017-10-09 19:53:51 +0700

    Create Styles.jsx

---
+++ constant/Styles.jsx
@@ -0,0 +1,166 @@
+import {blue500, red500,indigo900, blue800} from 'material-ui/styles/colors';
+
+export const styles = {
+  propContainer: {
+    width: 1200,
+    margin:20
+  },
+  tableHeader : {
+    color:indigo900,
+    fontSize:16
+  },
+  indexStyle:{
+    width:40
+  },
+  codeStyle:{
+    width:80
+  },
+  nameStyle:{
+    width:200
+  },
+  wrapper: {
+    display: 'flex',
+    flexWrap: 'wrap'
+  },
+  floatingLabelStyle :{
+      color : indigo900
+  },
+  noneDisplay:{
+    display : 'none'
+  },
+  headerWidth15 : {
+    width : 15,
+    color:indigo900,
+    fontSize:16
+  },
+  headerWidth20 : {
+    width : 20,
+    color:indigo900,
+    fontSize:16
+  },
+  headerWidth40 : {
+    width : 40,
+    color:indigo900,
+    fontSize:16
+  },
+  headerWidth50 : {
+    width : 50,
+    color:indigo900,
+    fontSize:16
+  }
+  ,
+  headerWidth60 : {
+    width : 60,
+    color:indigo900,
+    fontSize:16
+  },
+  headerWidthIndex : {
+    width : 64,
+    color:indigo900,
+    fontSize:16
+  },
+  headerWidth80 : {
+    width : 80,
+    color:indigo900,
+    fontSize:16
+  },
+  headerWidth100 : {
+    width : 100,
+    color:indigo900,
+    fontSize:16
+  },
+  headerWidth120 : {
+    width : 120,
+    color:indigo900,
+    fontSize:16
+  },
+  headerWidth140 : {
+    width : 140,
+    color:indigo900,
+    fontSize:16
+  },
+  headerWidth150 : {
+    width : 150,
+    color:indigo900,
+    fontSize:16
+  },
+  headerWidth200 : {
+    width : 200,
+    color:indigo900,
+    fontSize:16
+  },
+  width15 : {
+    width : 15
+  },
+  width20 : {
+    width : 20
+  },
+  width40:{
+    width:40
+  },
+  width50 : {
+    width : 50
+  },
+  width60 : {
+    width : 60
+  },
+  width64 : {
+    width : 64
+  },
+  width80:{
+    width:80
+  },
+  width100 : {
+    width : 100
+  },
+  width120 : {
+    width : 120
+  },
+  width140 : {
+    width : 140
+  },
+  width150 : {
+    width : 150
+  },
+  width200 : {
+    width : 200
+  },
+  fieldsetStyle: {
+    border: '1px solid #ddd'
+  },
+  floatRight: {
+    float: 'right'
+  },
+  margin20: {
+    margin: 20
+  },
+  titleStyle: {
+      backgroundColor: blue800
+    , color: 'white'
+    , lineHeight: 0
+    , marginBottom: 24
+  },
+  fieldsetStyle: {
+      padding: "5px 5px 5px 5px"
+    , border: '1px solid #ddd'
+  },
+  legendStyle: {
+    display: 'inherit'
+    , width: 'auto'
+    , padding: 'inherit'
+    , marginBottom: 'inherit'
+    , fontSize: 16
+    , lineHeight: 'inherit'
+    , color: indigo900
+    , border: 'none'
+  },
+  textCenter: {
+    textAlign: 'center'
+  },
+  textLeft: {
+    textAlign: 'left'
+  },
+  textRight: {
+    textAlign: 'right'
+  }
+};
