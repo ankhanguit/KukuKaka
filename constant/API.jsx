d31f6577e0f32dfaccc264e529f74bb39c403e52
ankhanguit <anhkhanguit@gmail.com>
2017-10-09 19:58:46 +0700

    Create API.jsx

---
+++ constant/API.jsx
@@ -0,0 +1,12 @@
+export const HOST = "http://localhost:8081/";
+export const PATH_MASTER = "kukukaka-master";
+export const PATH_PREPARATION = "kukukaka-preparation";
+export const PATH_PRINT = "kukukaka-print";
+//export const PATH_MASTER = "kukukaka";
+//export const PATH_PREPARATION = "kukukaka";
+//export const PATH_PRINT = "kukukaka";
+
+export const URL_FOLDER_IMAGE = `${HOST}medicineImages/`;
+
+export const SYSTEM_INIT = `${HOST+ PATH_MASTER}/api/v1/systemInit/`;
+export const LOGIN_URL = `${HOST+ PATH_MASTER}/api/v1/login/`;
